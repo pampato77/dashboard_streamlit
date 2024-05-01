744df9b8010b678eb5d0aed1e9640102cab985c2
pampato77 <141702046+pampato77@users.noreply.github.com>
2024-05-01 15:05:00 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,11 +4,14 @@ import streamlit as st
 
 from PIL import Image
 
-base = pd.read_csv('employeed.csv')
+
 st.title('Analisis de desempeño de los colaboradores') 
 st.header('Dashboard')
 st.text('Se analizaron los colaboradores de la empresa Clue ') 
 
 image = Image.open('sun.webp')
-st.image(image, caption='Logo de empresa',width=400px, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image(image, caption='Logo de empresa',width='400px', use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
+empleados = pd.read_csv('employeed.csv')
+#buscar los encabezados del csv
+#bdempleados = empleados[["name_employee","birth_date","age","gender","marital_status","hiring_date","position","salary","performance_score","last_performance_date","average_work_hours","satisfaction_level","absences"]]
