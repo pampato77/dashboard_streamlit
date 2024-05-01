1fd1645b916b6f3b88583c70b362f1e7f0257de0
pampato77 <141702046+pampato77@users.noreply.github.com>
2024-05-01 14:59:56 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,10 +4,10 @@ import streamlit as st
 
 from PIL import Image
 
-#base = pd.read_csv(“employeed.csv”)
+base = pd.read_csv('employeed.csv)
 st.title('Analisis de desempeño de los colaboradores') 
 st.header('Dashboard')
-st.text(“Se analizaron los colaboradores de la empresa Clue ”) 
+st.text('Se analizaron los colaboradores de la empresa Clue ') 
 
 image = Image.open('sun.webp')
 st.image(image, caption='Logo de empresa')
