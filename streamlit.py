33f5f427604e0f68fc55899447c0ac62e9d0058f
pampato77 <141702046+pampato77@users.noreply.github.com>
2024-05-01 14:50:53 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,6 +9,6 @@ st.title('Analisis de desempeño de los colaboradores')
 st.header('Dashboard')
 st.text(“Se analizaron los colaboradores de la empresa Clue ”) 
 
-image = Image.open('oso_panda.jpg')
+image = Image.open('sun.webp')
 st.image(image, caption='Logo de empresa')
 
