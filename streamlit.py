4ab5448983661b4276659aef3979c1c561017552
pampato77 <141702046+pampato77@users.noreply.github.com>
2024-05-01 15:00:20 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 from PIL import Image
 
-base = pd.read_csv('employeed.csv)
+base = pd.read_csv('employeed.csv')
 st.title('Analisis de desempeño de los colaboradores') 
 st.header('Dashboard')
 st.text('Se analizaron los colaboradores de la empresa Clue ') 
