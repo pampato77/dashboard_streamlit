f58dc5b3fb42a195cbe94e3d453b86c2777b20c1
pampato77 <141702046+pampato77@users.noreply.github.com>
2024-05-03 14:27:50 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,7 +4,6 @@ import streamlit as st
 
 from PIL import Image
 
-
 st.title('Analisis de desempeño de los colaboradores') 
 st.header('Dashboard')
 st.text('Se analizaron los colaboradores de la empresa Clue ') 
@@ -14,4 +13,8 @@ st.image(image, caption='SoftSun')
 
 empleados = pd.read_csv('employeed.csv')
 #buscar los encabezados del csv
-#bdempleados = empleados[["name_employee","birth_date","age","gender","marital_status","hiring_date","position","salary","performance_score","last_performance_date","average_work_hours","satisfaction_level","absences"]]
+bdempleados = empleados[["name_employee","birth_date","age","gender","marital_status","hiring_date","position","salary","performance_score","last_performance_date","average_work_hours","satisfaction_level","absences"]]
+
+#Control para seleccionar el genero del empleado
+Select_gen = st.selectbox("Seleccionar genero", bdempleados['gender'].unique()) 
+St.write(f"Selecciona una opción : { selected_gen!r }") 
