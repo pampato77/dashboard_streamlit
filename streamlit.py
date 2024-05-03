5c304970c71c7d9840994cccb31fd40d867002cd
pampato77 <141702046+pampato77@users.noreply.github.com>
2024-05-03 14:45:18 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -16,5 +16,8 @@ empleados = pd.read_csv('employeed.csv')
 bdempleados = empleados[["name_employee","birth_date","age","gender","marital_status","hiring_date","position","salary","performance_score","last_performance_date","average_work_hours","satisfaction_level","absences"]]
 
 #Control para seleccionar el genero del empleado
-Select_gen = st.selectbox("Seleccionar genero", bdempleados['gender'].unique()) 
-St.write(f"Selecciona una opción : { selected_gen!r }") 
+Select_gen = st.radio("Seleccionar genero", bdempleados['gender'].unique()) 
+St.write(f"Selecciona una opción : { Select_gen!r }") 
+
+#Control para seleccionar un rango del puntuaje
+
