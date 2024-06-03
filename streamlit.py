aed5566e0822b5c35dfb4c08fc509cb57caee641
pampato77 <141702046+pampato77@users.noreply.github.com>
2024-06-03 11:21:18 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import streamlit as st
+from bokeh.plotting import figure
 from PIL import Image
-#from bokeh.plotting import figure
 
 st.title('Analisis de desempeño de los colaboradores') 
 st.header('Dashboard')
@@ -10,13 +10,13 @@ st.text('Se analizaron los colaboradores de la empresa Clue ')
 image = Image.open('sunlogo.jpeg')
 st.image(image, caption='SoftSun')
 
-#empleados = pd.read_csv('employeed.csv')
+empleados = pd.read_csv('employeed.csv')
 #buscar los encabezados del csv
-#bdempleados = empleados[["name_employee","birth_date","age","gender","marital_status","hiring_date","position","salary","performance_score","last_performance_date","average_work_hours","satisfaction_level","absences"]]
+bdempleados = empleados[["name_employee","birth_date","age","gender","marital_status","hiring_date","position","salary","performance_score","last_performance_date","average_work_hours","satisfaction_level","absences"]]
 
 #Control para seleccionar el genero del empleado
-#Select_gen = st.radio("Seleccionar genero", bdempleados['gender'].unique()) 
-#st.write("Seleccionar género:", Select_gen) 
+Select_gen = st.radio("Seleccionar genero", bdempleados['gender'].unique()) 
+st.write("Seleccionar género:", Select_gen) 
 
 #Control para seleccionar un rango del puntuaje
 #Select_performance = st.radio("Seleccionar puntaje de desempeño", bdempleados['performance_score'].unique()) 
